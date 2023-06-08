658102cca9b02a0d87a79a47d8bebcc81e9b5408
bannzai <kingkong999yhirose@gmail.com>
2023-06-08 12:33:39 +0900

    Fix Package

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -10,11 +11,33 @@ let package = Package(
             name: "UtilityType",
             targets: ["UtilityType"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            branch: "main"
+        ),
+    ],
     targets: [
+        .macro(
+            name: "UtilityTypePlugin",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftOperators", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ],
+            path: "UtilityTypePlugin"
+        ),
         .target(
-            name: "UtilityType"),
+            name: "UtilityType",
+            dependencies: ["UtilityTypePlugin"],
+            path: ["UtilityType"]
+        ),
         .testTarget(
             name: "UtilityTypeTests",
-            dependencies: ["UtilityType"]),
+            dependencies: ["UtilityType"]
+        ),
     ]
 )
