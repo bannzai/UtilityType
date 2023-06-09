fb2db8f54e4a67fd078093e803ca9dfbf847972a
bannzai <kingkong999yhirose@gmail.com>
2023-06-09 10:33:13 +0900

    Add getter

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -1,12 +1,16 @@
 import Foundation
 import UtilityType
 
-@Pick("Picked", properties: "id", "name")
+@Pick("Picked", properties: "id", "name", "getter")
 @Pick("Picked2", properties: "name", "age")
 public struct User {
     let id: UUID
     let name: String
     let age: Int
+
+    var getter: String {
+        return "getter"
+    }
 }
 
 let user = User(id: .init(), name: "bannzai", age: 30)
