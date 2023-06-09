dc05fffb6ca38aba8bb5d4a80a9f99b163c6da86
bannzai <kingkong999yhirose@gmail.com>
2023-06-09 10:33:17 +0900

    Remove getter

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -1,16 +1,12 @@
 import Foundation
 import UtilityType
 
-@Pick("Picked", properties: "id", "name", "getter")
+@Pick("Picked", properties: "id", "name")
 @Pick("Picked2", properties: "name", "age")
 public struct User {
     let id: UUID
     let name: String
     let age: Int
-
-    var getter: String {
-        return "getter"
-    }
 }
 
 let user = User(id: .init(), name: "bannzai", age: 30)
