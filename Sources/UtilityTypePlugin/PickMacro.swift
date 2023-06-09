6e9a17c52fd28da269b7b7db05ee5013f82e48dc
bannzai <kingkong999yhirose@gmail.com>
2023-06-09 20:25:52 +0900

    Fix class

--- Sources/UtilityTypePlugin/PickMacro.swift
+++ Sources/UtilityTypePlugin/PickMacro.swift
@@ -102,25 +102,66 @@ public struct PickMacro: MemberMacro {
                 fatalError("Unexpected cast fail when kind == .classDecl")
             }
 
+            let className = declaration.identifier.text
+            let classVariableName = className.prefix(1).lowercased() + className.suffix(className.count - 1)
+
             let access = declaration.modifiers?.first(where: \.isNeededAccessLevelModifier)
-            let structProperties = declaration.memberBlock.members.children(viewMode: .all)
+            let classProperties = declaration.memberBlock.members.children(viewMode: .all)
                 .compactMap { $0.as(MemberDeclListItemSyntax.self) }
                 .compactMap { $0.decl.as(VariableDeclSyntax.self) }
                 .compactMap { $0.bindings.as(PatternBindingListSyntax.self) }
                 .compactMap {
-                    $0.children(viewMode: .all).compactMap { $0.as(PatternBindingSyntax.self) }
+                    $0.children(viewMode: .all)
+                        .compactMap { $0.as(PatternBindingSyntax.self) }
                 }
                 .flatMap { $0 }
-            let tupleElements = structProperties
-                .filter { structProperty in
-                    properties.contains { property in
-                        structProperty.pattern.as(IdentifierPatternSyntax.self)?.identifier.text == property
-                    }}
-                .map(\.description).joined(separator: ", ")
 
-            return [
-                "\(access)typealias \(name) = (_type: \(declaration.identifier.trimmed).Type, \(raw: tupleElements))",
-            ]
+            let targetClassProperties = classProperties
+                .filter { classProperty in
+                    properties.contains { property in
+                        classProperty.pattern.as(IdentifierPatternSyntax.self)?.identifier.text == property
+                    }}
+            let classRawProperties = targetClassProperties
+                .map { classProperty in
+                    let variableDecl = classProperty.parent!.parent!.cast(VariableDeclSyntax.self)
+                    let letOrVar = variableDecl.bindingKeyword.text
+                    if let access {
+                        return "\(access.description)\(letOrVar.trimmingPrefix(while: \.isWhitespace)) \(classProperty)"
+                    } else {
+                        return "\(letOrVar.trimmingPrefix(while: \.isWhitespace)) \(classProperty)"
+                    }
+                }
+                .joined()
+            let assignedToSelfPropertyStatementsFromDeclaration = targetClassProperties
+                .compactMap { classProperty in
+                    classProperty.pattern.as(IdentifierPatternSyntax.self)?.identifier.text
+                }
+                .map {
+                    "self.\($0) = \(classVariableName).\($0)"
+                }
+                .joined(separator: "\n")
+            let eachInitArgument = targetClassProperties
+                .map(\.description)
+                .joined(separator: ", ")
+            let assignedToSelfPropertyStatementsFromRawProperty = targetClassProperties
+                .compactMap { classProperty in
+                    classProperty.pattern.as(IdentifierPatternSyntax.self)?.identifier.text
+                }
+                .map {
+                    "self.\($0) = \($0)"
+                }
+                .joined(separator: "\n")
+
+            let syntax = try! ClassDeclSyntax("\(access)class \(name)", membersBuilder: {
+                DeclSyntax("\(raw: classRawProperties)")
+                try InitializerDeclSyntax("\(access)init(\(raw: classVariableName): \(raw: className))") {
+                    DeclSyntax("\(raw: assignedToSelfPropertyStatementsFromDeclaration)")
+                }
+                try InitializerDeclSyntax("\(access)init(\(raw: eachInitArgument))") {
+                    DeclSyntax("\(raw: assignedToSelfPropertyStatementsFromRawProperty)")
+                }
+            })
+            return [syntax.formatted().cast(DeclSyntax.self)]
         case _:
             throw CustomError.message("@Required can only be applied to a struct or class declarations.")
         }
