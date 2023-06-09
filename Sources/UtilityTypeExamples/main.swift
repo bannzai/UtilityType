83282916bb3192cac379c59247caa10d4d30e1e3
bannzai <kingkong999yhirose@gmail.com>
2023-06-09 20:40:39 +0900

    Revert "Test cascade macro"
    This reverts commit b21d7c2ccf923d5cabb11d7a6ab602773ae73a89.

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -7,14 +7,8 @@ public struct User {
     let id: UUID
     let name: String
     let age: Int
-
-    init(id: UUID, name: String, age: Int) {
-        self.id = id
-        self.name = name
-        self.age = age
-    }
 }
 
 let user = User(id: .init(), name: "bannzai", age: 30)
-let nest: User.Picked.Nest = User.Picked.Nest(picked: .init(user: user))
-print(nest.id)
+let pickedUser: User.Picked = .init(user: user)
+let picked2User: User.Picked2 = .init(user: user)
