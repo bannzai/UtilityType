f276bf7d28e2a811d38d07ed4a1a838c6d662256
bannzai <kingkong999yhirose@gmail.com>
2023-06-10 08:13:51 +0900

    add optional property

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -8,8 +8,9 @@ public struct User {
     let id: UUID
     let name: String
     let age: Int
+    let optional: Never?
 }
 
-let user = User(id: .init(), name: "bannzai", age: 30)
+let user = User(id: .init(), name: "bannzai", age: 30, optional: nil)
 let pickedUser: User.Picked = .init(user: user)
 let picked2User: User.Picked2 = .init(user: user)
