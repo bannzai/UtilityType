e6fb74b2867d3c24d3703a202f45f5d48acac913
bannzai <kingkong999yhirose@gmail.com>
2023-06-10 18:00:41 +0900

    Add cases

--- Sources/UtilityTypePlugin/Extension.swift
+++ Sources/UtilityTypePlugin/Extension.swift
@@ -28,3 +28,15 @@ extension String.StringInterpolation {
         }
     }
 }
+
+extension EnumDeclSyntax {
+    var cases: [EnumCaseElementSyntax] {
+        memberBlock.members.flatMap { member in
+            guard let caseDecl = member.decl.as(EnumCaseDeclSyntax.self) else {
+                return Array<EnumCaseElementSyntax>()
+            }
+
+            return Array(caseDecl.elements)
+        }
+    }
+}
