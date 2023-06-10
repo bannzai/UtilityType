1d14fe5ec66fe2853c2a420a9ae8e3314859008c
bannzai <kingkong999yhirose@gmail.com>
2023-06-10 10:51:39 +0900

    Remove ()

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -3,7 +3,7 @@ import UtilityType
 
 @Pick("Picked", properties: "id", "name")
 @Pick("Picked2", properties: "name", "age")
-@Required()
+@Required
 public struct User {
     let id: UUID
     let name: String
