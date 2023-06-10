ff6942b70687cb3eab52b13469feb80877e4fe28
bannzai <kingkong999yhirose@gmail.com>
2023-06-10 18:24:02 +0900

    Fix indent

--- Sources/UtilityTypePlugin/ExcludeMacro.swift
+++ Sources/UtilityTypePlugin/ExcludeMacro.swift
@@ -42,82 +42,37 @@ public struct ExcludeMacro: MemberMacro {
             cases.contains { c in enumCase.identifier.text == c }
         }
 
-        let syntax = try EnumDeclSyntax("\(access)enum \(typeName)",
-                                        membersBuilder: {
-            MemberDeclListSyntax(
-                excludedCases.map { excludedCase in
-                    MemberDeclListItemSyntax(
-                        decl: EnumCaseDeclSyntax(
-                            elements: EnumCaseElementListSyntax(
-                                [excludedCase]
+        let syntax = try EnumDeclSyntax(
+            "\(access)enum \(typeName)",
+            membersBuilder: {
+                MemberDeclListSyntax(
+                    excludedCases.map { excludedCase in
+                        MemberDeclListItemSyntax(
+                            decl: EnumCaseDeclSyntax(
+                                elements: EnumCaseElementListSyntax(
+                                    [excludedCase]
+                                )
                             )
                         )
-                    )
-                }
-            )
-            try InitializerDeclSyntax("\(access) init?(\(uniqueVariableName): \(typeName)") {
-                try CodeBlockItemListSyntax {
-                    try SwitchExprSyntax("switch \(uniqueVariableName)") {
-                        SwitchCaseListSyntax(try excludedCases.map {
-                            excludedCase in
-                            let identifier = excludedCase.identifier
-                            let parameters = excludedCase.associatedValue?.parameterList
-                            
-                            return .switchCase(
-                                SwitchCaseSyntax(
-                                    label: .case(
-                                        SwitchCaseLabelSyntax(
-                                            caseItems: CaseItemListSyntax(itemsBuilder: {
-                                                if let parameters {
-                                                    CaseItemSyntax(
-                                                        pattern: ExpressionPatternSyntax(
-                                                            expression: FunctionCallExprSyntax(
-                                                                calledExpression: MemberAccessExprSyntax(
-                                                                    name: identifier
-                                                                ),
-                                                                argumentList: TupleExprElementListSyntax(
-                                                                    parameters.map { parameter in
-                                                                        TupleExprElementSyntax(
-                                                                            expression: UnresolvedPatternExprSyntax(
-                                                                                pattern: ValueBindingPatternSyntax(
-                                                                                    bindingKeyword: TokenSyntax(
-                                                                                        stringLiteral: "let"
-                                                                                    ),
-                                                                                    valuePattern: IdentifierPatternSyntax(
-                                                                                        identifier: TokenSyntax(
-                                                                                            stringLiteral: "TODO"
-                                                                                        )
-                                                                                    )
-                                                                                )
-                                                                            )
-                                                                        )
-                                                                    }
-                                                                )
-                                                            )
-                                                        )
-                                                    )
-                                                } else {
-                                                    CaseItemSyntax(
-                                                        pattern: ExpressionPatternSyntax(
-                                                            expression: MemberAccessExprSyntax(
-                                                                name: identifier
-                                                            )
-                                                        )
-                                                    )
-                                                }
-                                            })
-                                        )
-                                    ),
-                                    statements: try CodeBlockItemListSyntax(itemsBuilder: {
-                                        if let parameters {
-                                            try CodeBlockItemSyntax(
-                                                item: .expr(
-                                                    SequenceExprSyntax(
-                                                        elements: ExprListSyntax(
-                                                            [
-                                                                IdentifierExprSyntax(identifier: .init(stringLiteral: "self")),
-                                                                AssignmentExprSyntax(assignToken: .equalToken()),
-                                                                FunctionCallExprSyntax(
+                    }
+                )
+                try InitializerDeclSyntax("\(access) init?(\(uniqueVariableName): \(typeName)") {
+                    try CodeBlockItemListSyntax {
+                        try SwitchExprSyntax("switch \(uniqueVariableName)") {
+                            SwitchCaseListSyntax(try excludedCases.map {
+                                excludedCase in
+                                let identifier = excludedCase.identifier
+                                let parameters = excludedCase.associatedValue?.parameterList
+
+                                return .switchCase(
+                                    SwitchCaseSyntax(
+                                        label: .case(
+                                            SwitchCaseLabelSyntax(
+                                                caseItems: CaseItemListSyntax(itemsBuilder: {
+                                                    if let parameters {
+                                                        CaseItemSyntax(
+                                                            pattern: ExpressionPatternSyntax(
+                                                                expression: FunctionCallExprSyntax(
                                                                     calledExpression: MemberAccessExprSyntax(
                                                                         name: identifier
                                                                     ),
@@ -140,37 +95,83 @@ public struct ExcludeMacro: MemberMacro {
                                                                         }
                                                                     )
                                                                 )
-                                                            ]
+                                                            )
                                                         )
-                                                    ).tryCast(ExprSyntax.self)
-                                                )
-                                            )
-                                        } else {
-                                            CodeBlockItemSyntax(
-                                                item: .expr(
-                                                    try SequenceExprSyntax(
-                                                        elements: ExprListSyntax(
-                                                            [
-                                                                IdentifierExprSyntax(identifier: .init(stringLiteral: "self")),
-                                                                AssignmentExprSyntax(),
-                                                                MemberAccessExprSyntax(
+                                                    } else {
+                                                        CaseItemSyntax(
+                                                            pattern: ExpressionPatternSyntax(
+                                                                expression: MemberAccessExprSyntax(
                                                                     name: identifier
                                                                 )
-                                                            ]
+                                                            )
                                                         )
-                                                    ).tryCast(ExprSyntax.self)
-                                                )
+                                                    }
+                                                })
                                             )
-                                        }
-                                    })
+                                        ),
+                                        statements: try CodeBlockItemListSyntax(itemsBuilder: {
+                                            if let parameters {
+                                                try CodeBlockItemSyntax(
+                                                    item: .expr(
+                                                        SequenceExprSyntax(
+                                                            elements: ExprListSyntax(
+                                                                [
+                                                                    IdentifierExprSyntax(identifier: .init(stringLiteral: "self")),
+                                                                    AssignmentExprSyntax(assignToken: .equalToken()),
+                                                                    FunctionCallExprSyntax(
+                                                                        calledExpression: MemberAccessExprSyntax(
+                                                                            name: identifier
+                                                                        ),
+                                                                        argumentList: TupleExprElementListSyntax(
+                                                                            parameters.map { parameter in
+                                                                                TupleExprElementSyntax(
+                                                                                    expression: UnresolvedPatternExprSyntax(
+                                                                                        pattern: ValueBindingPatternSyntax(
+                                                                                            bindingKeyword: TokenSyntax(
+                                                                                                stringLiteral: "let"
+                                                                                            ),
+                                                                                            valuePattern: IdentifierPatternSyntax(
+                                                                                                identifier: TokenSyntax(
+                                                                                                    stringLiteral: "TODO"
+                                                                                                )
+                                                                                            )
+                                                                                        )
+                                                                                    )
+                                                                                )
+                                                                            }
+                                                                        )
+                                                                    )
+                                                                ]
+                                                            )
+                                                        ).tryCast(ExprSyntax.self)
+                                                    )
+                                                )
+                                            } else {
+                                                CodeBlockItemSyntax(
+                                                    item: .expr(
+                                                        try SequenceExprSyntax(
+                                                            elements: ExprListSyntax(
+                                                                [
+                                                                    IdentifierExprSyntax(identifier: .init(stringLiteral: "self")),
+                                                                    AssignmentExprSyntax(),
+                                                                    MemberAccessExprSyntax(
+                                                                        name: identifier
+                                                                    )
+                                                                ]
+                                                            )
+                                                        ).tryCast(ExprSyntax.self)
+                                                    )
+                                                )
+                                            }
+                                        })
+                                    )
                                 )
-                            )
-                        })
+                            })
+                        }
                     }
                 }
-            }
-        })
-        
+            })
+
         return [syntax.formatted().cast(DeclSyntax.self)]
     }
 }
