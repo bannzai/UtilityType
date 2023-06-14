eddcab97b9291c997f08f28e98913978987d8fc5
bannzai <kingkong999yhirose@gmail.com>
2023-06-14 13:39:16 +0900

    Add Pick case

--- Tests/UtilityTypePluginTests/PickTests.swift
+++ Tests/UtilityTypePluginTests/PickTests.swift
@@ -1,8 +1,51 @@
-//
-//  File.swift
-//  
-//
-//  Created by bannzai on 2023/06/14.
-//
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+import UtilityTypePlugin
+import XCTest
 
-import Foundation
+final class PickTests: XCTestCase {
+    func testMacro() throws {
+        let sourceFile: SourceFileSyntax =
+      #"""
+      @Pick("Pickted", properties: "id", "name")
+      public struct User {
+          let id: UUID
+          let name: String
+          let age: Int
+          let optional: Void?
+      }
+      """#
+        let context = BasicMacroExpansionContext.init(
+            sourceFiles: [sourceFile: .init(moduleName: "MyModule", fullFilePath: "test.swift")]
+        )
+        let expanded = sourceFile.expand(macros: [
+            "Pick": PickMacro.self
+        ], in: context)
+
+        XCTAssertEqual(
+            expanded.formatted().description,
+      #"""
+
+      public struct User {
+          let id: UUID
+          let name: String
+          let age: Int
+          let optional: Void?
+          public struct Pickted {
+              public let id: UUID
+              public let name: String
+              public init(user: User) {
+                  self.id = user.id
+                  self.name = user.name
+              }
+              public init(id: UUID, name: String) {
+                  self.id = id
+                  self.name = name
+              }
+          }
+      }
+      """#
+        )
+    }
+}
