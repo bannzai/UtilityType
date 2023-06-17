a816fe27c7cff2e0b5c5d0c40f88de9c9738b4e3
bannzai <kingkong999yhirose@gmail.com>
2023-06-17 20:16:25 +0900

    Add failure test case

--- Tests/UtilityTypePluginTests/PickTests.swift
+++ Tests/UtilityTypePluginTests/PickTests.swift
@@ -8,7 +8,7 @@ final class PickTests: XCTestCase {
     func testMacro() throws {
         let sourceFile: SourceFileSyntax =
       #"""
-      @Pick("Pickted", properties: "id", "name")
+      @Pick("Pickted", properties: "id", "name", macros: "@Required")
       public struct User {
           let id: UUID
           let name: String
