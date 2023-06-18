5b6e498ff0c639e658159d813afa232c19de739a
bannzai <kingkong999yhirose@gmail.com>
2023-06-18 12:40:54 +0900

    add example code

--- Sources/UtilityTypeExamples/main.swift
+++ Sources/UtilityTypeExamples/main.swift
@@ -12,6 +12,7 @@ public struct Item {
 @Omit("Omitted", properties: "id")
 @Pick("Picked", properties: "id", "name")
 @Pick("PickedNest", properties: "id", "name", macros: #"@Required"#, #"@Partial"#, #"@Omit("Omitted", properties: "id")"#)
+@Omit("OmittedNest", properties: "name", macros: #"@Required"#, #"@Partial"#, #"@Pick("Picked", properties: "id")"#)
 public struct User {
     let id: UUID
     let name: String
@@ -26,6 +27,7 @@ let pickedUser = User.Picked(id: UUID(), name: "bannzai")
 let pickedNestMacroRequierd = User.PickedNest.Required(id: .init(), name: "bannzai")
 let pickedNestMacroPartial = User.PickedNest.Partial(id: .init(), name: "bannzai")
 let pickedNestMacroOmit = User.PickedNest.Omitted(name: "bannzai")
+let omittedNestPick = User.OmittedNest.Picked(id: .init())
 let omittedUser = User.Omitted(name: "bannzai", age: 30, optional: nil)
 
 @Exclude("ExcludedThree", exlcudes: "three")
