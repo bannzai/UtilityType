61c4b656615498cbc6d248872467415a9f2d1a7d
bannzai <kingkong999yhirose@gmail.com>
2023-06-18 13:00:14 +0900

    Fix Partial test

--- Tests/UtilityTypePluginTests/PartialTests.swift
+++ Tests/UtilityTypePluginTests/PartialTests.swift
@@ -54,4 +54,55 @@ final class PartialTests: XCTestCase {
       """#
         )
     }
+
+    func testMacroNest() throws {
+        let sourceFile: SourceFileSyntax =
+      #"""
+      @Partial(macros: #"@Pick("Picked", properties: "id")"#)
+      public struct User {
+          let id: UUID
+          let name: String
+          let age: Int
+          let optional: Void?
+      }
+      """#
+        let context = BasicMacroExpansionContext.init(
+            sourceFiles: [sourceFile: .init(moduleName: "MyModule", fullFilePath: "test.swift")]
+        )
+        let expanded = sourceFile.expand(macros: [
+            "Partial": PartialMacro.self
+        ], in: context)
+
+        XCTAssertEqual(
+            expanded.formatted().description,
+      #"""
+
+      public struct User {
+          let id: UUID
+          let name: String
+          let age: Int
+          let optional: Void?
+          @Pick("Picked", properties: "id")
+          public struct Partial {
+              public let id: UUID?
+              public let name: String?
+              public let age: Int?
+              public let optional: Void?
+              public init(user: User) {
+                  self.id = user.id
+                  self.name = user.name
+                  self.age = user.age
+                  self.optional = user.optional
+              }
+              public init(id: UUID?, name: String?, age: Int?, optional: Void?) {
+                  self.id = id
+                  self.name = name
+                  self.age = age
+                  self.optional = optional
+              }
+          }
+      }
+      """#
+        )
+    }
 }
