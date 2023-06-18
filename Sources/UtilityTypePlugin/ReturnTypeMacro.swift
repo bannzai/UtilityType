b56fd0b0a577359ad4e653f1ba4a78c2f566f9d0
bannzai <kingkong999yhirose@gmail.com>
2023-06-18 13:08:02 +0900

    Fix return type

--- Sources/UtilityTypePlugin/ReturnTypeMacro.swift
+++ Sources/UtilityTypePlugin/ReturnTypeMacro.swift
@@ -16,16 +16,27 @@ public struct ReturnTypeMacro: PeerMacro {
         else {
             throw CustomError.message(#"@ReturnType requires the type name, in the form @ReturnType("ReturnType")"#)
         }
-
         guard let funcDecl = declaration.as(FunctionDeclSyntax.self) else {
             throw CustomError.message("@ReturnType should attach to `function`)")
         }
 
+        let _macros: [String]?
+        if let macrosIndex = arguments.firstIndex(where: { $0.label?.text == "macros"}) {
+            _macros = arguments[macrosIndex...]
+                .map(\.expression)
+                .compactMap { $0.as(StringLiteralExprSyntax.self) }
+                .flatMap { $0.segments.children(viewMode: .all) }
+                .compactMap { $0.as(StringSegmentSyntax.self) }
+                .flatMap { $0.tokens(viewMode: .all) }
+                .map(\.text)
+        } else {
+            _macros = nil
+        }
+        let macros = _macros?.joined(separator: "\n") ?? ""
+
         let access = funcDecl.modifiers?.first(where: \.isNeededAccessLevelModifier)
-
         let returnType = funcDecl.signature.output?.returnType ?? TypeSyntax(stringLiteral: "Void")
-
-        return [try StructDeclSyntax("\(access)struct \(name)") {
+        return [try StructDeclSyntax("\(raw: macros)\n\(access)struct \(name)") {
             DeclSyntax("\(access)\(raw: "typealias RawValue = \(returnType)")")
             DeclSyntax("\(access)\(raw: "let rawValue: RawValue")")
             try InitializerDeclSyntax("\(access)init(\(raw: "rawValue: RawValue"))", bodyBuilder: {
