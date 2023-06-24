89d7f291c3250d6d96abe974b50c72c1bac77c5f
bannzai <kingkong999yhirose@gmail.com>
2023-06-24 21:51:21 +0900

    Add constructor parameters

---
+++ Sources/UtilityTypePlugin/ConstructorParametersMacro.swift
@@ -0,0 +1,72 @@
+import SwiftSyntax
+import SwiftSyntaxMacros
+import SwiftSyntaxBuilder
+
+public struct ConstructorParametersMacro: PeerMacro {
+    public static func expansion<Context, Declaration>(
+        of node: AttributeSyntax,
+        providingPeersOf declaration: Declaration,
+        in context: Context
+    ) throws -> [DeclSyntax] where Context : MacroExpansionContext, Declaration : DeclSyntaxProtocol {
+        guard
+            case .argumentList(let arguments) = node.argument, arguments.count > 0,
+            let string = arguments.first?.expression.as(StringLiteralExprSyntax.self),
+            string.segments.count == 1,
+            let name = string.segments.first 
+        else {
+            throw CustomError.message(#"@ConstructorParameters requires the type name, in the form @ConstructorParameters("TypeName")"#)
+        }
+
+        guard let initDecl = declaration.as(InitializerDeclSyntax.self) else {
+            throw CustomError.message("@ConstructorParameters should attach to `function`)")
+        }
+
+        let access = initDecl.modifiers?.first(where: \.isNeededAccessLevelModifier)
+
+        let parameters = initDecl.signature.input.parameterList.children(viewMode: .all)
+            .compactMap { $0.as(FunctionParameterSyntax.self) }
+            .map { functionParameter in
+                if let attributedFunctionParameter = functionParameter.type.as(AttributedTypeSyntax.self) {
+                    // for c: @escaping () -> Void
+                    return FunctionParameterSyntax(
+                        firstName: functionParameter.firstName,
+                        type: attributedFunctionParameter.baseType
+                    )
+                } else {
+                    return functionParameter
+                }
+            }
+
+        let nameText = try name.text
+        let initParams = TypealiasDeclSyntax(
+            modifiers: ModifierListSyntax(access != nil ? [access!] : []),
+            identifier: TokenSyntax(stringLiteral: nameText),
+            initializer: TypeInitializerClauseSyntax(
+                value:
+                    TupleTypeSyntax(
+                        elements: TupleTypeElementListSyntax(parameters.indices.map { index in
+                            let parameter = parameters[index]
+                            return TupleTypeElementSyntax(
+                                name: parameter.firstName,
+                                colon: .colonToken(),
+                                type: parameter.type,
+                                trailingComma: index + 1 == parameters.count ? nil : .commaToken()
+                            )
+                        })
+                    )
+            )
+        )
+
+        let variableName = nameText.prefix(1).lowercased() + nameText.suffix(nameText.count - 1)
+        let assignedToSelfPropertyStatementsFromDeclaration = parameters
+            .map(\.firstName.text)
+            .map { "self.\($0) = \(variableName).\($0)" }
+            .joined(separator: "\n")
+
+        let initFunc = try InitializerDeclSyntax("\(access)init(\(raw: variableName): \(raw: nameText))") {
+            DeclSyntax("\(raw: assignedToSelfPropertyStatementsFromDeclaration)")
+        }
+
+        return try [initParams.tryCast(DeclSyntax.self), initFunc.tryCast(DeclSyntax.self)]
+    }
+}
