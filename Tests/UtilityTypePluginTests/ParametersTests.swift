34bd5d5aa83d23a36628ceb7ef101601a0b4ea4a
bannzai <kingkong999yhirose@gmail.com>
2023-06-25 12:32:36 +0900

    Fix ParametersTests

--- Tests/UtilityTypePluginTests/ParametersTests.swift
+++ Tests/UtilityTypePluginTests/ParametersTests.swift
@@ -2,33 +2,27 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import UtilityTypePlugin
+import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 final class ParametersTests: XCTestCase {
     func testMacro() throws {
-        let sourceFile: SourceFileSyntax =
+        assertMacroExpansion(
       #"""
       @Parameters("FunctionArgs")
       func function(a: Int, b: String, c: @escaping () -> Void, e: () -> Void) -> Int {
           return 1
       }
-      """#
-        let context = BasicMacroExpansionContext.init(
-            sourceFiles: [sourceFile: .init(moduleName: "MyModule", fullFilePath: "test.swift")]
-        )
-        let expanded = sourceFile.expand(macros: [
-            "Parameters": ParametersMacro.self
-        ], in: context)
-
-        XCTAssertEqual(
-            expanded.formatted().description,
+      """#,
+      expandedSource:
       #"""
 
       func function(a: Int, b: String, c: @escaping () -> Void, e: () -> Void) -> Int {
           return 1
       }
       typealias FunctionArgs = (a: Int, b: String, c: () -> Void, e: () -> Void)
-      """#
+      """#,
+      macros: ["Parameters": ParametersMacro.self]
         )
     }
 }
